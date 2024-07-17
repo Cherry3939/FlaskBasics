824995e10d263286e908bc4ca8da44632876e770
Cherry <158606375+Cherry3939@users.noreply.github.com>
2024-07-17 16:00:16 +0800

    Add files via upload
    rahhh

---
+++ flask basics 2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/chickens")
+def chickens():
+    return render_template("chickens.html")
+
+@app.route("/pandas")
+def pandas():
+    return render_template("pandas.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=12345)
